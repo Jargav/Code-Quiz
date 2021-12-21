dc099df0722831ca299eadf69a8f5348534cb741
Jared Gavigan <jagavigan80@gmail.com>
2021-12-21 15:10:12 -0500

    code quiz attempt

---
+++ assets/js/questions.js
@@ -0,0 +1,47 @@
+var questions = [
+    { 
+        question: "Commonly used data types DO NOT include?", 
+        answers: [
+            { text: "strings", correct: false },
+            { text: "booleans" , correct: false },
+            { text: "alerts", correct: true },
+            { text: "numbers", correct: false }
+        ]
+    },
+    { 
+        question: "The condition in an if/else statement is enclosed within _____?", 
+        answers: [
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "parentheses", correct: true },
+            { text: "square brackets", correct: false }
+        ]
+    },
+    { 
+        question: "Arrays in JavaScript can be used to store ____?", 
+        answers: [
+            { text: "numbers and strings", correct: false },
+            { text: "othet arrays", correct: false },
+            { text: "booleans", correct: false },
+            { text: "all of the above", correct: true }
+        ]
+    },
+    { 
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:?", 
+        answers: [
+            { text: "JavaScript", correct: false },
+            { text: "terminal/bash", correct: false },
+            { text: "for loops", correct: false },
+            { text: "console.log", correct: true }
+        ]
+    },
+    { 
+        question: "How do you round the number 102.456, to the nearest integer?",
+        answers: [
+            { text: "Math.random(102.456)", correct: false },
+            { text: "Math.rnd(102.456)", correct: false },
+            { text: "round(102.456)", correct: false },
+            { text: "None of the above", correct: true }
+        ]
+    },
+];
